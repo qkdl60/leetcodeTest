98a81f2ec6f0ae4c7d4844a19a885a87d7030d4e
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-08-14 09:46:23 +0900

    Time: 48 ms (99.93%), Space: 45.4 MB (66.71%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.ts
@@ -0,0 +1,24 @@
+/**
+평션 배열이 주어졋을때, 새로운 펑션을 리턴, 이걸 펑션 조합, 펑선들의 배열
+[fx, gx, hx] 의 조합은 fn(x)=f(g(h(x)))
+배열이 비어잇다. f(x)=x
+
+ */
+
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+	return function(x) {
+     if(functions.length===0)return x   
+     while(functions.length){
+       let a =functions.pop()
+       x=a(x)
+     }
+      return x
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
